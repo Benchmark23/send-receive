0d4e0610cfd4bca73366b7f1a01dd64930d6eba3
Sharpx <sharpx-@outlook.com>
2023-10-31 02:40:01 +0800

    feat: add args

--- manager/recv_manager.cc
+++ manager/recv_manager.cc
@@ -6,13 +6,25 @@
 #include "receiver.h"
 #include "sender.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <taskfile> <logfile> <self_port>" << std::endl;
+        return 1;
+    }
+
+    std::string taskfile = argv[1];
+    std::string logfile = argv[2];
+
+    int self_port = std::atoi(argv[3]);
+
     std::vector<entry> recv_entries;
-    parse("assignment/task_receive.log",recv_entries);
+    parse(taskfile, recv_entries);
 
     TCPReceiver receiver;
     int connect_socket,server_socket;
-    receiver.accept__(connect_socket,server_socket,8081);
+    receiver.accept__(connect_socket,server_socket,self_port);
 
     int i=0;
     while(i<recv_entries.size()){
@@ -22,8 +34,7 @@ int main() {
     }
     receiver.disconnect__(connect_socket,server_socket);
 
-    flush("results/result_recv",receiver.kvs);
-
+    flush(logfile, receiver.kvs);
 
     return 0;
 }
--- manager/send_manager.cc
+++ manager/send_manager.cc
@@ -6,12 +6,25 @@
 #include "utils.h"
 #include "receiver.h"
 #include "sender.h"
-int main() {
+int main(int argc, char *argv[])
+{
 
-    
+    if (argc != 6)
+    {
+        std::cerr << "Usage: " << argv[0] << " <taskfile> <logfile> <target_ip> <self_port> <target_port>" << std::endl;
+        return 1;
+    }
+
+    std::string taskfile = argv[1];
+    std::string logfile = argv[2];
+    std::string target_ip = argv[3];
+
+    int self_port = std::atoi(argv[4]);
+    int target_port = std::atoi(argv[5]);
+
     std::vector<entry> send_entries;
     std::vector<long long> intvals;
-    parse("assignment/task_send.log",send_entries);
+    parse(taskfile, send_entries);
 
     for(int i = 0; i < send_entries.size(); i++){
         long long nanoseconds;
@@ -28,7 +41,7 @@ int main() {
     int t_client_socket=0;
     UDPSender u_sender;
     
-    t_sender.connect__(t_client_socket,"127.0.0.1",8081);
+    t_sender.connect__(t_client_socket, target_ip, target_port);
 
     for(int i=0;i<send_entries.size();i++){
 
@@ -52,8 +65,7 @@ int main() {
     }
 
     t_sender.disconnect__(t_client_socket);
-    flush("results/result_send",t_sender.kvs);
-
+    flush(logfile, t_sender.kvs);
 
     return 0;
 }
